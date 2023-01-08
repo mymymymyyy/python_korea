7938dce685874f10137afc88ac0eb5ef433aac64
mymymymyyy <bgusang12@gmail.com>
2023-01-08 10:05:50 +0900

    hello

---
+++ day01_8.py
@@ -0,0 +1,19 @@
+#   대입연산자
+a = 0
+print(a)
+
+# 누적연산자
+a += 3           #a = a + 3
+print(a)
+
+a +=2           #a = a + 2
+print(a)
+
+a + 2
+print(a)
+
+a - 4
+print(a)       #a 값이 변경 안됨      a - 4
+
+a -=4           #a 값이 변경됨       a = a - 4
+print(a)
