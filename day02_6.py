de1122ef240c32548bd08301f1bbd1b40427dec4
mymymymyyy <bgusang12@gmail.com>
2023-01-08 13:33:35 +0900

    파이썬 2일차

---
+++ day02_6.py
@@ -0,0 +1,13 @@
+#조건문  if
+# 반복문 while,for
+
+# if : 만약에 ~라ㅏ면, 조건식이 맞으면 실행
+#while : ~ 동안, 조건식이 맞는 동안에 계속 실행
+
+if True:
+    print('조건문 실행')            # 얘를 실행한 다음 밑으로 내려감
+print('if끝남')
+
+while True:
+    print('반복문 실행')            # 얘를 실행한 다음 위로 돌아감(while) --> 조건을 다시 검사
+print('while끝남')          
